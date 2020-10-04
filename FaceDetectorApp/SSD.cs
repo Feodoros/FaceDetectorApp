75bdea8736fbfc384dc7dbd45390e951fa7d3d8e
Fedor Zhilkin <fzhilkin@mail.ru>
2020-10-05 01:51:27 +0300

    Initial commit for SSD with TF

---
+++ FaceDetectorApp/SSD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceDetectorApp
+{
+    public sealed class SSD
+    {
+
+    }
+}
